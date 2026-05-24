e26fc0b4c7723d799e24348bc89f0664fb081ea3
Muhammed Musthafa <90796311+muhammedmusthafatp@users.noreply.github.com>
2026-05-24 16:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome CI/CD pipelines!"
+    return "Welcome CI/CD pipelines updated!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
